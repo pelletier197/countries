e5bfaa3e241765a515a15adb65bd1483e06157d0
Sunny Pelletier <sunny.pelletier@qohash.com>
2022-08-18 20:40:45 -0400

    Updates

---
+++ magidoc.mjs
@@ -0,0 +1,20 @@
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  introspection: {
+    type: "sdl",
+    paths: [path.join(__dirname, "../schema.graphqls")],
+  },
+  website: {
+    template: "carbon-multi-page",
+    staticAssets: path.join(__dirname, "./assets"),
+    output: path.join(__dirname, "./build"),
+    options: {
+      appLogo:
+        "https://raw.githubusercontent.com/trevorblades/countries/main/logo.png",
+    },
+  },
+};
